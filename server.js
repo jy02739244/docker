6fac57301fafc3c526ebed0951c7cf45b6248d8e
iori <duanke26@gmail.com>
2016-03-15 13:08:34 +0800

    'port'

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
 // server.js
-var http = require('http')
+var express = require('express')
+var app=express();
+app.get('',function(req,res){
+	res.send('Hello');
+});
 
-var server = http.createServer(function(req, res) {
-  res.writeHead(200, {
-    'Content-Type': 'text/plain'
-  })
-  res.end('Hello World!')
-})
-
-server.listen(8080, function() {
+app.listen(80, function() {
   console.log('Docker DEMO with Node.js is running.')
 })
